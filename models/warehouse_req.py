72f85b6918f8bedaa05e76fb15b99955c3c46191
AfroMonkey <moisalejandro@gmail.com>
2017-03-15 16:59:27 -0600

    [IMP] Selection fields list added

--- models/warehouse_req.py
+++ models/warehouse_req.py
@@ -6,62 +6,74 @@ from odoo import api, fields, models
 class WarehouseReq(models.Model):
     _name = 'warehouse.req'
 
-    name = fields.Char(index=True, string="Folio", readonly=True)  # TODO autoincrement
+    name = fields.Char(index=True, string='Folio', readonly=True)  # TODO autoincrement
     warehouse = fields.Many2one(
-        comodel_name="stock.warehouse",
+        comodel_name='stock.warehouse',
         required=True,
-        # ondelete=  # TODO
-        # default=  # TODO
     )
     purchase_required = fields.Boolean(compute='_purchase_required')
     date_request = fields.Date(
         default=fields.Date.today,
         readonly=True,
-        string="Date of Request",
+        string='Date of Request',
     )
     date_required = fields.Date()  # TODO check date_required >= date_request
     reason = fields.Selection(
         selection=[
-            # TODO
+            ('production', 'Production'),
+            ('stock_cs', 'Stock CS'),
+            ('loan', 'Loan'),
+            ('warranty', 'Warranty'),
+            ('sale', 'Sale'),
+            ('reparation', 'Reparation'),
+            ('replacement', 'Replacement'),
+            ('internal', 'Internal Use'),
+            ('integration', 'Integration'),
+            ('minimal', 'Minimal Supplieres'),
         ],
         required=True,
     )
     reference_type = fields.Selection(
         selection=[
-            # TODO
+            ('support', 'Support'),
+            ('kickoff', 'KickOff'),
+            ('project', 'Project'),
+            ('others', 'Others'),
+            ('bill', 'Bill of Materials'),
         ],
     )
     reference_folio = fields.Integer()
     claimant_id = fields.Many2one(
-        comodel_name="res.users",
+        comodel_name='res.users',
         # default= # TODO current user
-        # ondelete=  # TODO
         readonly=True,
         required=True,
-        string="Claimant",
+        string='Claimant',
     )
     state = fields.Selection(
         selection=[
+            ('required', 'Required'),
+            ('approved', 'Approved'),
             # TODO
         ]
     )
     shipping_type = fields.Selection(
         selection=[
-            # TODO
+            ('next', 'Next day'),
+            ('other', 'Other'),
         ],
     )
     client_id = fields.Many2one(
         comodel_name="res.partner",
         domain="[('customer', '=', True)]",
-        string="Client",
-        # ondelete=  # TODO
+        string='Client',
     )
     deliver_to = fields.Char()  # TODO field type
     deliver_address = fields.Char()  # TODO field type
     product_ids = fields.One2many(
-        comodel_name="warehouse.req.product",
-        inverse_name="warehouse_req_id",
-        string="Products",
+        comodel_name='warehouse.req.product',
+        inverse_name='warehouse_req_id',
+        string='Products',
     )
 
     def _purchase_required(self):
