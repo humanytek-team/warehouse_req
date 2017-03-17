d1f9ba7dd2d606b1408b15d8c5893dc3673bdaca
AfroMonkey <moisalejandro@gmail.com>
2017-03-16 18:36:44 -0600

    [IMP] product_ids tree

--- models/warehouse_req_product.py
+++ models/warehouse_req_product.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models
 class WarehouseReqProduct(models.Model):
     _name = 'warehouse.req.product'
 
+    sequence = fields.Integer()
     warehouse_req_id = fields.Many2one(
         comodel_name="warehouse.req",
         index=True,
@@ -11,14 +12,18 @@ class WarehouseReqProduct(models.Model):
         required=True,
         string="Requirement",
     )
+    name = fields.Char(related='product_id.name')
     product_id = fields.Many2one(
         comodel_name="product.template",  # TODO template vs product
         domain="[('purchase_ok', '=', True)]",
         required=True,
     )
-    # description  # TODO need?
-    specs = fields.Text()
-    # on_hand  # TODO need?
+    specs = fields.Char()
+    on_hand  = fields.Float(
+        default=lambda self: self.product_id.virtual_available,
+        readonly=True,
+        store=False,
+    )
     requested_qty = fields.Integer(required=True)
     auothorized_qty = fields.Integer()  # readonly if no authorized
     ordered_qty = fields.Integer(readonly=True)
