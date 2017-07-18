1c27f20ff5628d905f8c0ee9f87dfd1d0266a5be
AfroMonkey <moisalejandro@gmail.com>
2017-07-18 13:02:54 -0500

    [FIX][IMP][1.13.0] account_analytic only in debit

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Warehouse Req',
-    'version': '1.12.2',
+    'version': '1.13.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
     'depends': [
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -5,8 +5,8 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     @api.multi
-    def do_new_transfer(self):
-        res = super(StockPicking, self).do_new_transfer()
+    def do_transfer(self):
+        res = super(StockPicking, self).do_transfer()
         for pick in self:
             warehouse_req_id = self.env['warehouse.req'].search([('name', '=', pick.origin)], limit=1)
             if warehouse_req_id:
@@ -16,5 +16,6 @@ class StockPicking(models.Model):
                         stock_move_ids = [stock_move.id for stock_move in pick.move_lines]
                         account_move_lines = pick.env['account.move.line'].search([('stock_move_id', 'in', stock_move_ids)])
                         for account_move_line in account_move_lines:
-                            account_move_line.analytic_account_id = account_analytic_id
+                            if account_move_line.debit > 0 and account_move_line.credit == 0:
+                                account_move_line.analytic_account_id = account_analytic_id
         return res
